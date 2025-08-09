2d5fcebb9e3d1b836e910776e360a8e9d7760a07
jakoojavo <jakoojavo@gmail.com>
2025-08-08 23:52:30 -0300

    correccion

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import db from "./config/db";
-import  routerAgente  from "./router/agenteinmobiliario";
+import  routerAgente  from "./router/agenteInmobiliario.router";
 
 const server = express();
 server.use(express.json());
@@ -8,6 +8,18 @@ server.use(express.json());
 
 server.use("/api",routerAgente);
 
+// async function connectDB() {
+//    try {
+//        await db.authenticate(); // Verificar conexión con la base de datos
+//        console.log("Conexión a la base de datos exitosa");
+
+//        await db.sync({ force: true }); // Sincronizar tablas (¡Cuidado con force en producción!)
+//        console.log("Tablas sincronizadas");
+//      } catch (error) {
+//        console.error("Error al conectar o sincronizar la base de datos:", error);
+//      }
+//    }
+//   connectDB()
 
 
 async function conectDB() {
